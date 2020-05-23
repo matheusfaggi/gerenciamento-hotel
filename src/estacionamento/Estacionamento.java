dc14cfdaf4d1bcb18bd9aa6ebc27ed5ad26c99e2
Matheus Faggi <matheusfaggi@gmail.com>
2020-05-23 02:42:49 -0300

    Comentando e indentando o codigo

--- src/estacionamento/Estacionamento.java
+++ src/estacionamento/Estacionamento.java
@@ -15,28 +15,41 @@ import veiculos.*;
  * @author matheusfaggi
  */
 public class Estacionamento {
-    private ArrayList<Vaga> vagas;
+    private final ArrayList<Vaga> vagas;
     public Estacionamento(ArrayList<Vaga> vagas){
         this.vagas = vagas;
     }
     public void listVagas() {
-        for(Vaga vaga : vagas){
+        for(Vaga vaga : vagas) {
+//          Percorre todas vagas, e exibe que veiculo pertenceu a vaga 
             System.out.println(String.format("Vaga %d: \n " , vaga.getNumero()));
-            for(Object veiculo : vaga.getVeiculos()){
-                if(veiculo instanceof Bicicleta){
+            for(Object veiculo : vaga.getVeiculos()) {
+            /*
+                Descobre o tipo do objeto
+                Converte ela para ter acesso aos métodos
+                
+                Utiliza eles
+                
+                - Desafio: reduzir quantidade de IFs, 
+                    passando a resposabilidade para a classe veiculo
+            */
+                if(veiculo instanceof Bicicleta) {
                     Bicicleta bike = (Bicicleta)veiculo;
+                    
                     System.out.println(String.format(
                         "Cor: %s \nqtdLugar: %d \nMarcha: %s\n\n",
                         bike.getCor(), bike.getQtdLugar(), bike.isMarcha()));
                 }
-                if(veiculo instanceof Carro){
+                if(veiculo instanceof Carro) {
                     Carro carro = (Carro)veiculo;
+                    
                     System.out.println(String.format(
                         "Cor: %s \nqtdLugar: %d \nModelo/ano: %s/%d\n\n",
                         carro.getCor(), carro.getQtdLugar(), carro.getModelo(), carro.getAno()));
                 }
-                if(veiculo instanceof Motocicleta){
+                if(veiculo instanceof Motocicleta) {
                     Motocicleta moto =  (Motocicleta)veiculo;
+                    
                     System.out.println(String.format(
                         "Cor: %s \nqtdLugar: %d \nCilindrada: %s\n\n",
                         moto.getCor(), moto.getQtdLugar(), moto.getCilindrada()));
