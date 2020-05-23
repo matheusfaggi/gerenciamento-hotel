2faf3248c99495051d1451e1b6cd3cf5d30b059c
Matheus Faggi <matheusfaggi@gmail.com>
2020-05-23 01:17:49 -0300

    Implementando testes no main

--- src/gerenciamento/hotel/GerenciamentoHotel.java
+++ src/gerenciamento/hotel/GerenciamentoHotel.java
@@ -5,6 +5,11 @@
  */
 package gerenciamento.hotel;
 
+import estacionamento.*;
+import veiculos.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author matheusfaggi
@@ -15,7 +20,30 @@ public class GerenciamentoHotel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Bicicleta bike =  new Bicicleta("preto");
+        Motocicleta moto = new Motocicleta("Verde", 2);
+        moto.setCilindrada(200);
+        Carro carro = new Carro("Cinza", 5);
+        carro.setAno(2012);
+        carro.setModelo("Uno Fiat");
+        
+        Vaga v1 = new Vaga(1);
+        v1.addBicicleta(bike);
+        v1.addMotocicleta(moto);
+        
+        Vaga v2 = new Vaga(2);
+        v2.addCarro(carro);
+        
+        ArrayList<Vaga> vagas = new ArrayList(Arrays.asList(v1,v2));
+        
+        Estacionamento estacionamento =  new Estacionamento(vagas);
+        
+        estacionamento.addVaga(v1);
+        estacionamento.addVaga(v2);
+        
+        estacionamento.listVagas();
+        
+        
     }
     
 }
