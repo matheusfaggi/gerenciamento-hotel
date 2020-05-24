c4c0fd7fc568da5a4c3141a13e10479efcebcec1
Matheus Faggi <matheusfaggi@gmail.com>
2020-05-24 16:28:53 -0300

    Adicionando collections de cliente

--- src/pessoa/Cliente.java
+++ src/pessoa/Cliente.java
@@ -4,15 +4,23 @@
  * and open the template in the editor.
  */
 package pessoa;
-import java.time.LocalDate;
+import hospedagem.Hospedagem;
+import hospedagem.Reserva;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author matheusfaggi
  */
-public class Cliente extends Pessoa{
+public class Cliente extends Pessoa {
     private String email;
-    private LocalDate ultimaHospedagem;
+    private Date ultimaHospedagem;
+    private final List reservas = new <Reserva> ArrayList();
+    private final Map <Integer, Hospedagem> hospedagens = new <Integer, Hospedagem> HashMap();
     
     public Cliente(String nome) {
         super(nome);
@@ -26,12 +34,25 @@ public class Cliente extends Pessoa{
         this.email = email;
     }
 
-    public LocalDate getUltimaHospedagem() {
+    public Date getUltimaHospedagem() {
         return ultimaHospedagem;
     }
 
-    public void setUltimaHospedagem(LocalDate ultimaHospedagem) {
-        this.ultimaHospedagem = ultimaHospedagem;
+    private void setUltimaHospedagem() {
+//        System.out.println(this.hospedagens.size());
+        
+//        Hospedagem ultima = this.hospedagens.get();
+//        this.ultimaHospedagem =  ultima.getSaida();
+    }
+    
+    public void addHospedagem(Hospedagem hospedagem) {
+        hospedagens.put(this.hospedagens.size() + 1, hospedagem);
+        this.setUltimaHospedagem();
+    }
+    
+    public void addReserva(Reserva reserva){
+        this.reservas.add(reserva);
+        this.setUltimaHospedagem();
     }
     
 }
