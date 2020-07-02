e5a1b1f8b41b04c9d5a1cbe0a5daed0ade65f0dc
Matheus Faggi <matheusfaggi@gmail.com>
2020-07-02 16:12:16 -0300

    create database class

---
+++ src/hotel/database/Database.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author matheusfaggi
+ */
+public class Database {
+    public static String database, user, password, host;
+    public static int port = 3306;
+    private static Connection connection;  
+    
+    static {
+        database = "gerenciamento_hotel";
+        user = "root";
+        password = "Faggi@123";
+        host = "localhost";        
+    }
+    
+    public static Connection getConnection(){
+        return connection;
+    }
+    
+    public static void open() throws SQLException{
+        String url = "jdbc:mysql://" + host +
+                     ":" + port + "/" + database;
+        
+        connection = DriverManager.getConnection(url, user, password);
+    }
+    
+    public static void close() throws SQLException{
+        connection.close();
+    }
+}
